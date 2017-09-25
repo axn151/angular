359984d31bede2b782e8bee8973b1869af0ef567
felipe <axn151@gmail.com>
2017-09-25 10:57:49 -0300

    ultimo

--- models/notasModel.js
+++ models/notasModel.js
@@ -1,35 +1,29 @@
 //modelo notasModel
 //un servicio viene a ser un modelo
-Agenda.service("notasModel", function(){
- 
-    this.getNotas = function(){
-        return [
-            {
-                id : 0,
-                nombre : "Israel Parra",
-                edad : "32 años"
-            },
-            {
-                id : 1,
-                nombre : "Andrés Cuenca",
-                edad : "24 años"
-            },
-            {
-                id : 2,
-                nombre : "Juan",
-                edad : "28 años"
-            },
-            {
-                id : 3,
-                nombre : "Pepito",
-                edad : "18 años"
-            },
-            {
-                id : 4,
-                nombre : "Manuel",
-                edad : "45 años"
+Agenda.service("notasModel", function () {
+
+    this.getNotas = function () {
+
+        var resultado;
+
+        $.ajax({
+                type: "GET",
+                // data: persona,
+                url: 'http://localhost:8001/api/users',
+                success: function (data) {
+                    resultado = data;
+
+                },
+                error: function (e) {
+
+                    resultado = null;
+                }
+
             }
-        ]
+
+        );
+
+        alert(resultado);
+        return resultado;
     }
- 
 });
